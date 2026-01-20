f38033403b1e0a8a3f1f85a77c0a25ef6d9786e6
UPM-UNH <planificacion@unh.edu.pe>
2026-01-20 16:37:54 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// ID del Google Sheets
+
+const GOOGLE\_SHEET\_ID = "14\_ROarwRyo5tsdjQZeMa\_hY1TVuyJtHrXzgpILDf9NM";
+
+
+
+// GID de la pestaña principal
+
+const GOOGLE\_SHEET\_GID = "1276577330";
+
+
+
+// URL CSV
+
+const SHEET\_CSV\_URL = `https://docs.google.com/spreadsheets/d/${GOOGLE\\\_SHEET\\\_ID}/export?format=csv\\\&gid=${GOOGLE\\\_SHEET\\\_GID}`;
